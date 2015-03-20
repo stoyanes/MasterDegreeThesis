af557c2f85ce56e7aebad5ca17074e02ee30fbc6
Stoyan Stoyanov <djstoxer@gmail.com>
2015-03-20 11:07:22 +0200

    Some folders movings inside solution

---
+++ src/TImeOffTrackingSystem/Server.Data/Repositories/IBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data.Repositories
+{
+    public interface IBaseRepository<T>
+     where T : class
+    {
+        int Add(T theType);
+
+        System.Collections.Generic.IList<T> GetAll();
+
+        System.Collections.Generic.IList<T> GetAllByIds(int[] ids);
+
+        T GetById(int entityId);
+
+        bool GetExists(int entityId);
+
+        bool Remove(int entityId);
+
+        bool Remove(T item);
+
+        bool Save(T theType);
+
+        bool Update(T entity);
+    }
+}

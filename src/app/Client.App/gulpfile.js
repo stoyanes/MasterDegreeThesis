5c3a73f41375c8d9e73fdc272f2d8ed717d1e48d
Stoyan Stoyanov <djstoxer@gmail.com>
2015-10-31 22:10:06 +0200

    stylish jshint

--- src/app/Client.App/gulpfile.js
+++ src/app/Client.App/gulpfile.js
@@ -5,14 +5,15 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
 
 gulp.task('linter', function () {
     var fileGlob = 'app/**/*.js';
     return gulp.src(fileGlob)
       .pipe(watch(fileGlob))
       .pipe(jshint())
-      .pipe(jshint.reporter('default'));
+      .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('default', function () {

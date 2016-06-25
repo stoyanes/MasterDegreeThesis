d6ae434133562fa70655782a4c4a89d62043a4d8
Stoyan Stoyanov <stoyanes@gmail.com>
2016-06-25 17:29:53 +0300

    added simple print button and refacotred repeateds code

--- src/app/Client.App/app/js/controllers/applicationController.js
+++ src/app/Client.App/app/js/controllers/applicationController.js
@@ -53,5 +53,27 @@
                     }
                     return vacationTypeName
                 };
+
+                $rootScope.getStatusName = function (status) {
+                    var statusName = '';
+                    switch (status) {
+                        case 1: statusName = 'Submitted'; break;
+                        case 2: statusName = 'Approved'; break;
+                        case 3: statusName = 'Rejected'; break;
+                        default: statusName = 'None'; break;
+                    }
+                    return statusName;
+                };
+
+                $rootScope.getCssClass = function (userRequest) {
+                    var cssClass = '';
+                    switch (userRequest.status) {
+                        case 1: cssClass = 'list-group-item-info'; break;
+                        case 2: cssClass = 'list-group-item-success'; break;
+                        case 3: cssClass = 'list-group-item-danger'; break;
+                        default: cssClass = 'list-group-item-warning'; break;
+                    }
+                    return cssClass;
+                };
             }]); // end of controller
     }); // end of define
--- src/app/Client.App/app/js/controllers/userRequestsController.js
+++ src/app/Client.App/app/js/controllers/userRequestsController.js
@@ -6,26 +6,6 @@
         app.controller('UserRequestsController', [
             '$rootScope','$scope',
             function ($rootScope, $scope) {
-                $scope.getStatusName = function (status) {
-                    var statusName = '';
-                    switch(status) {
-                        case 1: statusName = 'Submitted'; break;
-                        case 2: statusName = 'Approved'; break;
-                        case 3: statusName = 'Rejected'; break;
-                        default: statusName = 'None'; break;
-                    }
-                    return statusName;
-                };
 
-                $scope.getCssClass = function (userRequest) {
-                    var cssClass = '';
-                    switch(userRequest.status){
-                        case 1: cssClass = 'list-group-item-info'; break;
-                        case 2: cssClass = 'list-group-item-success'; break;
-                        case 3: cssClass = 'list-group-item-danger'; break;
-                        default: cssClass = 'list-group-item-warning'; break;
-                    }
-                    return cssClass;
-                };
             }]); // end of controller
     }); // end of define
